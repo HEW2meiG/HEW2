b217f6bbba40ba0d23b7401d8aa8ab6fcc682e72
YashiN1910 <yashintools@gmail.com>
2021-02-05 12:36:45 +0900

    出品履歴ver.1

--- flmapp/views/history.py
+++ flmapp/views/history.py
@@ -28,13 +28,13 @@ bp = Blueprint('history', __name__, url_prefix='/mypage/history')
 def sell_on_display():
     """出品中履歴"""
     user_id = current_user.get_id()
-    next_url = prev_url = None
+    next_url_1 = prev_url_1 = items = None
     page = request.args.get('page', 1, type=int)
     posts = Sell.select_sell_by_deal_status(user_id, 1, page)
-    next_url = url_for('app.sell_in_progress', page=posts.next_num, user_id=user.id) if posts.has_next else None
-    prev_url = url_for('app.sell_in_progress', page=posts.prev_num, user_id=user.id) if posts.has_prev else None
+    next_url_1 = url_for('history.sell_on_display', page=posts.next_num) if posts.has_next else None
+    prev_url_1 = url_for('history.sell_on_display', page=posts.prev_num) if posts.has_prev else None
     items = posts.items
-    return render_template('history/sell_history.html', items=items, next_url=next_url, prev_url=prev_url)
+    return render_template('history/sell_history.html', items=items, next_url_1=next_url_1, prev_url_1=prev_url_1)
 
 
 @bp.route('/sell_in_progress', methods=['GET', 'POST'])
@@ -42,15 +42,13 @@ def sell_on_display():
 def sell_in_progress():
     """出品取引中履歴"""
     user_id = current_user.get_id()
-    items = Sell.select_sell_by_deal_status(user_id, 2, 1)
-    next_url = prev_url = None
-    if items:
-        page = request.args.get('page', 1, type=int)
-        posts = Sell.select_sell_by_deal_status(user.id, 2, page)
-        next_url = url_for('app.sell_in_progress', page=posts.next_num, user_id=user.id) if posts.has_next else None
-        prev_url = url_for('app.sell_in_progress', page=posts.prev_num, user_id=user.id) if posts.has_prev else None
+    next_url_2 = prev_url_2 = items = None
+    page = request.args.get('page', 1, type=int)
+    posts = Sell.select_sell_by_deal_status(user_id, 2, page)
+    next_url_2 = url_for('history.sell_in_progress', page=posts.next_num) if posts.has_next else None
+    prev_url_2 = url_for('history.sell_in_progress', page=posts.prev_num) if posts.has_prev else None
     items = posts.items
-    return render_template('history/sell_history.html', items=items, next_url=next_url, prev_url=prev_url)
+    return render_template('history/sell_history.html', items=items, next_url_2=next_url_2, prev_url_2=prev_url_2)
 
 
 @bp.route('/sell_completed', methods=['GET', 'POST'])
@@ -58,15 +56,13 @@ def sell_in_progress():
 def sell_completed():
     """出品取引済み履歴"""
     user_id = current_user.get_id()
-    items = Sell.select_sell_by_deal_status(user_id, 3, 1)
-    next_url = prev_url = None
-    if items:
-        page = request.args.get('page', 1, type=int)
-        posts = Sell.select_sell_by_deal_status(user.id, 3, page)
-        next_url = url_for('app.sell_in_progress', page=posts.next_num, user_id=user.id) if posts.has_next else None
-        prev_url = url_for('app.sell_in_progress', page=posts.prev_num, user_id=user.id) if posts.has_prev else None
+    next_url_3 = prev_url_3 = items = None
+    page = request.args.get('page', 1, type=int)
+    posts = Sell.select_sell_by_deal_status(user_id, 3, page)
+    next_url_3 = url_for('history.sell_completed', page=posts.next_num) if posts.has_next else None
+    prev_url_3 = url_for('history.sell_completed', page=posts.prev_num) if posts.has_prev else None
     items = posts.items
-    return render_template('history/sell_history.html', items=items, next_url=next_url, prev_url=prev_url)
+    return render_template('history/sell_history.html', items=items, next_url_3=next_url_3, prev_url_3=prev_url_3)
 
 
 @bp.route('/buy_in_progress', methods=['GET', 'POST'])
